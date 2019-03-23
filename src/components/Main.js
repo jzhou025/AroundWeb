100825b6c37301b401144196b58be81ea81bc717
jzhou025 <jingyuzhou77@gmail.com>
2019-03-23 15:22:13 -0700

    add Main and Register component

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Register } from './Register';
+
+export class Main extends Component {
+    render () {
+        return (
+            <div className="main">
+                <Register/>
+            </div>
+        );
+    }
+}
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';  
+import logo from '../assets/images/logo.svg';
+
+
+export class TopBar extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />           
+            <span className="App-title">Around</span>
+          </header>
+        );
+    }
+}
