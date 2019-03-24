fb3f55cf5154f44ca1efbbedb43dc8fac964a60e
jzhou025 <jingyuzhou77@gmail.com>
2019-03-24 14:08:16 -0700

    add Home and Login module

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Register } from './Register';
+import { Login } from './Login';
+import { Home } from './Home';
 
 export class Main extends Component {
     render () {
         return (
             <div className="main">
-                <Register/>
+                <Register />
+                <Login />
+                <Home />
             </div>
         );
     }
