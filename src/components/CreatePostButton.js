9ede0db4eac08ae83bc6f4ef6b7d870303fd604e
jzhou025 <jingyuzhou77@gmail.com>
2019-04-01 19:22:00 -0700

    creat post skeleton

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Modal, Button } from 'antd';
+import { CreatPostForm } from './CreatePostForm';
 
 export class CreatePostButton extends React.Component {
+    state = {
+        visible: false,
+        confirmLoading: false,
+    }
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleOk = () => {
+        this.setState({
+            confirmLoading: true,
+        });
+        setTimeout(() => {
+            this.setState({
+                visible: false,
+                confirmLoading: false,
+            });
+        }, 2000);
+    }
+
+    handleCancel = () => {
+        console.log('Clicked cancel button');
+        this.setState({
+            visible: false,
+        });
+    }
+
     render() {
+        const { visible, confirmLoading } = this.state;
         return (
-            <Button>Create Posts</Button>
+            <div>
+                <Button type="primary" onClick={this.showModal}>
+                    Create New Post
+                </Button>
+                <Modal
+                    title="Create New Post"
+                    visible={visible}
+                    onOk={this.handleOk}
+                    confirmLoading={confirmLoading}
+                    onCancel={this.handleCancel}
+                    okText="Create"
+                >
+                    <CreatPostForm />
+                </Modal>
+            </div>
         );
     }
 }
---
+++ src/components/CreatePostForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class CreatPostForm extends React.Component {
+    render() {
+        return (
+            <div>CreatPostForm</div>
+        );
+    }
+}
